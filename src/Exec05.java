28077b92cae333a60ed4712e380f0cb2fe8e96f6
joalissonmarques <joalisonmarques@hotmail.com>
2022-10-24 17:49:40 -0300

    commit main

--- src/Exec05.java
+++ src/Exec05.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Exec05 {
     public static void main(String[] args) throws Exception {
         /* Escreva um algoritmo que leia o nome de um aluno e as notas das três provas que ele obteve no semestre.
         No final informar o nome do aluno e a sua média (aritmética); */
+
+        Scanner scan = new Scanner(System.in);
     }
 }
